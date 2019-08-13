a807ee630f8a101b86dd56913e6a0439dbbca037
Kiranchawla09 <kiranchawla09@gmail.com>
2019-08-13 23:05:42 +0530

    Hashing

---
+++ Count distinct elements in every window.cpp
@@ -0,0 +1,15 @@
+void countDistinct(int A[], int k, int n)
+{
+    int count=0;
+    int arr1[k];
+    for (int i=0;i<=n-k;i++)
+    {
+          unordered_set<int>mp;
+        for (int j=i;j<i+k;j++)
+        {
+            mp.insert(A[j]);
+        }
+        cout<< mp.size()<< " ";
+    }
+    //Your code here
+}

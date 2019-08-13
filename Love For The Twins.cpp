ae62a9f8531e9705888b1898584745f1a6a8b06e
Kiranchawla09 <kiranchawla09@gmail.com>
2019-08-13 11:35:13 +0530

    Hashing

---
+++ Love For The Twins.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+		int t;
+	cin>>t;
+	while (t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    unordered_map<int,int>mp;
+	    for (int i=0;i<n;i++)
+	    {
+	       cin>> arr[i];
+	       mp[arr[i]]++;
+	    }
+	    int count=0;
+	    for (auto itr=mp.begin();itr!= mp.end();itr++)
+	    {
+	    int l= (itr->second)/2;
+	    count+=l;  
+	    }
+	    cout<< count*2 << "\n";
+	}
+	return 0;
+}

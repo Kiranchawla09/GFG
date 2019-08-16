2717d1d34367b1e053294f2662e0962968920900
Kiranchawla09 <kiranchawla09@gmail.com>
2019-08-16 15:58:43 +0530

    Template

---
+++ C++ Generic sort.cpp
@@ -0,0 +1,16 @@
+template <class T>
+void sortArray(T a[], int n)
+{ 
+  std::sort (a,a+n);  
+	//Add your code here.
+}
+template <class T>
+void printArray(T a[], int n)
+{
+	//Add your code here.
+	
+	for (int i=0;i<n;i++)
+	cout<< a[i] <<" ";
+	
+	cout<< "\n";
+}

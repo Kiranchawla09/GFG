ca8551628d13e1a3f44281ac2fdec8c6b30c6e27
Kiranchawla09 <kiranchawla09@gmail.com>
2019-08-16 13:01:07 +0530

    Binary Tree

---
+++ Foldable Binary Tree.cpp
@@ -0,0 +1,18 @@
+int same(node *n1,node*n2)
+{
+if(n1==NULL && n2==NULL)
+return 1;
+if((n1!=NULL && n2==NULL) ||(n1==NULL && n2!=NULL) )
+return 0;
+return ((same(n1->left,n2->left)) && (same(n1->right,n2->right)));
+}
+bool isFoldable(struct node *root)
+{
+if(root==NULL)
+return true;
+mirror(root->left);
+int x=same(root->left,root->right);
+if(x==1)
+return true;
+return false;
+}

185bb96ca6603e7d7a5d87700de028366e9bbf0f
Kiranchawla09 <kiranchawla09@gmail.com>
2019-08-17 12:36:44 +0530

    Bit Magic

---
+++ Arya and The Great War.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	
+	int t;
+	cin>> t;
+	while (t--)
+	{
+	    int n;
+	    cin>> n;
+	    
+	    int count_bits=0;
+	    while (n != 1)
+	    {
+        if (n % 2)
+        count_bits++;
+        n=n/2;
+        }
+        cout<< count_bits+1<< "\n";
+        }
+	return 0;
+}
